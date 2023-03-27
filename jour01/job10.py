8fca0b12250c43e2785b17fb17681a6b4150e9cf
S. A <salvatore.anania@laplateforme.io>
2023-03-27 11:53:37 +0200

    modification du dernier exercice

--- jour01/job10.py
+++ jour01/job10.py
@@ -1,10 +1,11 @@
 class Voiture:
     def __init__(self):
-        self.__marque
-        self.__modele
-        self.__annee
-        self.__kilométrage 
+        self.__marque="generique"
+        self.__modele="generique"
+        self.__annee=2020
+        self.__kilométrage=50000
         self.__en_marche=False
+        self.__reservoir=50
     
     def get_marque(self):
         return self.__marque
@@ -19,7 +20,13 @@ class Voiture:
         return self.__kilométrage
     
     def get_enMarche(self):
-        return self.__en_marche
+        if self.__en_marche:
+            return "en marche"
+        else:
+            return "à l'arrêt"
+        
+    def get_reservoir(self):
+        return self.__reservoir
     
     def set_marque(self,marque):
         self.__marque=marque
@@ -33,5 +40,29 @@ class Voiture:
     def set_kilometrage(self,kilométrage):
         self.__kilométrage=kilométrage
     
-    def set_enMarche(self):
+    def change_enMarche(self):
         self.__en_marche=not self.__en_marche
+    
+    def set_reservoir(self,litre):
+        self.__reservoir=litre
+        
+    def demarrer(self):
+        if self.get_enMarche()=="à l'arrêt" and self.verifier_plein()>5:
+            self.change_enMarche()
+        
+    def arreter(self):
+        if self.get_enMarche()=="en marche":
+            self.change_enMarche()
+        
+    def verifier_plein(self):
+        return self.__reservoir
+    
+    def info(self):
+        print(f"Voiture de marque {self.get_marque()}, modele {self.get_modele()}, datant de {self.get_annee()} avec {self.get_kilometrage()} kilometres,qui est {self.get_enMarche()} et le reservoir est à {self.get_reservoir()}")
+    
+voiture=Voiture()
+voiture.info()
+voiture.demarrer()
+voiture.info()
+voiture.arreter()
+voiture.info()
