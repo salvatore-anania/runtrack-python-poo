46284c8e9fc3ebd800bb9a2ad7b36d8127b92357
S. A <salvatore.anania@laplateforme.io>
2023-03-28 19:54:45 +0200

    ajout interface graphique du dernier exercice

--- jour02/job07.py
+++ jour02/job07.py
@@ -2,15 +2,16 @@ from random import randint
 import pygame
 
 class Carte:
-    def __init__(self,couleur,valeur):
+    def __init__(self,couleur,valeur,image):
         self.__couleur=couleur
         self.__valeur=valeur
+        self.__image=image
         
     def get_valeur(self):
         return self.__valeur
     
-    def affiche(self):
-        print( f"{self.__valeur} de {self.__couleur}")
+    def affiche(self,screen,x,y):
+        screen.blit(self.__image,(x,y))
     
 class Jeu:
     def __init__(self):
@@ -21,16 +22,16 @@ class Jeu:
         change_color=0
         for i in range(1,53):
             if i%13==1:
-                self.__paquet.append(Carte(couleur[change_color],11))
+                self.__paquet.append(Carte(couleur[change_color],11,pygame.image.load(f"card/{couleur[change_color]}/{couleur[change_color]}A.png")))
             elif i%13==0:
-                self.__paquet.append(Carte(couleur[change_color],10))
+                self.__paquet.append(Carte(couleur[change_color],10,pygame.image.load(f"card/{couleur[change_color]}/{couleur[change_color]}K.png")))
                 change_color+=1
             elif i%13==11:
-                self.__paquet.append(Carte(couleur[change_color],10))
+                self.__paquet.append(Carte(couleur[change_color],10,pygame.image.load(f"card/{couleur[change_color]}/{couleur[change_color]}V.png")))
             elif i%13==12:
-                self.__paquet.append(Carte(couleur[change_color],10))
+                self.__paquet.append(Carte(couleur[change_color],10,pygame.image.load(f"card/{couleur[change_color]}/{couleur[change_color]}Q.png")))
             else:
-                self.__paquet.append(Carte(couleur[change_color],i%13))
+                self.__paquet.append(Carte(couleur[change_color],i%13,pygame.image.load(f"card/{couleur[change_color]}/{couleur[change_color]}{i%13}.png")))
         return self.__paquet
     
     def get_paquet(self):
@@ -55,9 +56,10 @@ class Main():
     def piocher(self,carte):
         self.__main.append(carte)
         
-    def affiche(self):
+    def affiche(self,screen,x=0,y=0):
         for carte in self.__main:
-            carte.affiche()
+            carte.affiche(screen,x,y)
+            x+=80
             
     def calcul_main(self):
         total_main=0
@@ -68,40 +70,75 @@ class Main():
     
     def main_plus_forte(self,main):
         if self.calcul_main()<main.calcul_main() and main.calcul_main()<=21:
-            print("Vous avez perdu")
+            return font.render("vous avez perdu !", True, pygame.Color((63,72,204)))
         else:
-            print("Vous avez gagne")
+            return font.render("vous avez gagné !", True, pygame.Color((63,72,204)))
+pygame.init()
 
-paquet=Jeu()
-joueur=Main()
-croupier=Main()
-paquet.remplir_paquet()
-piocher=True
-perdu=False
-for i in range (2):
-    joueur.piocher(paquet.distribuer())
-    croupier.piocher(paquet.distribuer())
-while piocher:
-    print("Votre main :")
-    joueur.affiche()
-    print("tirer une carte : oui(O) non(N)")
-    if input()=="O":
+screen = pygame.display.set_mode((800,600))
+back=pygame.image.load("card/background.png")
+back_replay=pygame.image.load("card/background_replay.png")
+pygame.display.set_caption("space invaders")
+font = pygame.font.SysFont("calibri", 50, bold=True)
+
+running=True
+while running:
+    paquet=Jeu()
+    joueur=Main()
+    croupier=Main()
+    paquet.remplir_paquet()
+    piocher=True
+    perdu=False
+    screen.blit(back,(0,0))
+    
+    for i in range (2):
         joueur.piocher(paquet.distribuer())
-    else:
-        piocher=False
-if joueur.calcul_main()>21:
-    perdu=True
-piocher=True
-
-while piocher and not perdu:
-    print("\nla main du croupier :")
-    croupier.affiche()
-    if croupier.calcul_main()<17:
         croupier.piocher(paquet.distribuer())
+    while piocher:
+        joueur.affiche(screen,200,355)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                piocher=False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.mouse.get_pos()[0] in range(181,360) and pygame.mouse.get_pos()[1] in range(470,540):
+                    joueur.piocher(paquet.distribuer())
+                    if joueur.calcul_main()>21:
+                        perdu=True
+                        piocher=False
+                elif pygame.mouse.get_pos()[0] in range(457,651) and pygame.mouse.get_pos()[1] in range(470,540):
+                    piocher=False
+            pygame.display.update()
+    piocher=True
+    
+    while piocher and not perdu and running:
+        croupier.affiche(screen,200,90)
+        if croupier.calcul_main()<17:
+            croupier.piocher(paquet.distribuer())
+        else:
+            piocher=False
+ 
+                 
+    if not perdu:
+        texte=joueur.main_plus_forte(croupier)
     else:
-        piocher=False
-
-if not perdu:
-    joueur.main_plus_forte(croupier)
-else:
-    print("Vous avez perdu")
+        texte = font.render("vous avez perdu !", True, pygame.Color((63,72,204)))
+        
+    screen.blit(back_replay,(0,0))
+    screen.blit(texte, (400-(texte.get_width()/2), 250))
+    croupier.affiche(screen,200,90)
+    joueur.affiche(screen,200,355)
+    end=True
+    while end and running:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                end=False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                    if pygame.mouse.get_pos()[0] in range(181,360) and pygame.mouse.get_pos()[1] in range(470,540):
+                        end=False
+                    elif pygame.mouse.get_pos()[0] in range(457,651) and pygame.mouse.get_pos()[1] in range(470,540):
+                        running=False
+                    
+        pygame.display.update()
